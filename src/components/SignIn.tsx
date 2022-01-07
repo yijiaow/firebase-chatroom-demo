1e70a5cde3e48b6a3efd4ea85628c85d7e2e299e
Yijiao Wang <ttwyj250@gmail.com>
2022-01-06 17:00:13 -0800

    Catch error auth/account-exists-with-different-credential, link multiple providers

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -9,7 +9,11 @@ import {
   GithubAuthProvider,
   signInWithPopup,
   signInWithCustomToken,
+  OAuthProvider,
+  fetchSignInMethodsForEmail,
+  linkWithCredential,
 } from 'firebase/auth';
+import { FirebaseError } from 'firebase/app';
 import { AuthContext } from '../contexts/AuthContext';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../lib/firebase';
@@ -25,20 +29,9 @@ const SignIn: React.FC<SignInProps> = ({ location }) => {
   const { auth } = useContext(AuthContext);
   const history = useHistory();
 
-  const signInWithProvider = async (authProvider: string) => {
-    let provider;
-    switch (authProvider) {
-      case 'google':
-        provider = new GoogleAuthProvider();
-        break;
-      case 'twitter':
-        provider = new TwitterAuthProvider();
-        console.log('clicked Twitter sign in');
-        break;
-      case 'github':
-        provider = new GithubAuthProvider();
-        break;
-    }
+  const signInWithProvider = async (providerId: string) => {
+    const provider = getProviderForProviderId(providerId);
+
     auth.useDeviceLanguage();
 
     try {
@@ -52,15 +45,48 @@ const SignIn: React.FC<SignInProps> = ({ location }) => {
           name: user.displayName,
           email: user.email,
           photoUrl: user.photoURL,
-          authProvider: authProvider,
+          providerId,
         },
         { merge: true }
       );
 
       history.push(location.state.referrer as string);
     } catch (err: unknown) {
-      if (err instanceof Error)
-        console.error(`Things exploded! ${err.message}`);
+      if (
+        err instanceof FirebaseError &&
+        err.code === 'auth/account-exists-with-different-credential'
+      ) {
+        const email = err.customData!.email as string;
+        const pendingCredential = OAuthProvider.credentialFromError(err);
+
+        fetchSignInMethodsForEmail(auth, email).then((methods) => {
+          var provider = getProviderForProviderId(methods[0]);
+          // TODO: Let the user know that they already have an account with a different provider,
+          // and let them validate the fact they want to sign in with this provider.
+
+          // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
+          // so in real scenario you should ask the user to click on a "continue" button
+          // that will trigger the signInWithPopup.
+          if (pendingCredential) {
+            signInWithPopup(auth, provider).then((result) =>
+              linkWithCredential(result.user, pendingCredential)
+            );
+          }
+        });
+      }
+    }
+  };
+
+  const getProviderForProviderId = (providerId: string) => {
+    switch (providerId) {
+      case 'google.com':
+        return new GoogleAuthProvider();
+      case 'twitter.com':
+        return new TwitterAuthProvider();
+      case 'github.com':
+        return new GithubAuthProvider();
+      default:
+        return new GoogleAuthProvider();
     }
   };
 
@@ -82,26 +108,26 @@ const SignIn: React.FC<SignInProps> = ({ location }) => {
     }
   };
 
-  useEffect(() => {
-    signInWithToken();
-  }, []);
+  // useEffect(() => {
+  //   signInWithToken();
+  // }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-full">
       <button
-        onClick={() => signInWithProvider('google')}
+        onClick={() => signInWithProvider('google.com')}
         className="flex justify-center items-center pl-6 pr-8 py-3 rounded shadow-button focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-75 bg-white hover:bg-gray-50 text-gray-600 font-medium"
       >
         Sign In with Google
       </button>
       <button
-        onClick={() => signInWithProvider('twitter')}
+        onClick={() => signInWithProvider('twitter.com')}
         className="flex justify-center items-center pl-6 pr-8 py-3 rounded shadow-button focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-75 bg-white hover:bg-gray-50 text-gray-600 font-medium"
       >
         Sign In with Twitter
       </button>
       <button
-        onClick={() => signInWithProvider('github')}
+        onClick={() => signInWithProvider('github.com')}
         className="flex justify-center items-center pl-6 pr-8 py-3 rounded shadow-button focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-75 bg-white hover:bg-gray-50 text-gray-600 font-medium"
       >
         Sign In with GitHub
